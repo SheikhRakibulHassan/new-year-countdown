a00532ed3743d3b0db3799b70444ac401b1f406a
SheikhRakibulHassan <42910970+SheikhRakibulHassan@users.noreply.github.com>
2024-06-09 23:55:39 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const dayEl = document.getElementById("day");
+
+const hourEl = document.getElementById("hour");
+
+const minuteEl = document.getElementById("minute");
+
+const secondEl = document.getElementById("second");
+
+const newYearTime = new Date("Jan 1, 2024 00:00:00").getTime();
+
+updateCountdown();
+
+function updateCountdown() {
+    const now = new Date().getTime();
+    const gap = newYearTime - now;
+
+
+    const second =1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const d = Math.floor(gap/day);
+    const h = Math.floor()
+}
